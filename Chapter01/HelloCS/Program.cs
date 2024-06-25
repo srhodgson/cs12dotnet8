bfcae753dc8a29463a0db2e488a1a322489ca1b9
Stephen Hodgson <stephen@srhodgson.co.uk>
2024-06-25 22:25:13 +0100

    Add simple program to display enviroment details

--- Chapter01/HelloCS/Program.cs
+++ Chapter01/HelloCS/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, C#!");
+string name = typeof(Program).Namespace ?? "None!";
+Console.WriteLine($"Namespace: {name}");
+throw new Exception();
