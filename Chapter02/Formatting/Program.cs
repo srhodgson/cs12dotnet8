b55a17bcbe63e0b2633035a73079114622a426cf
Stephen Hodgson <stephen@srhodgson.co.uk>
2024-06-27 22:23:23 +0100

    Formatting using named and unnamed params

--- Chapter02/Formatting/Program.cs
+++ Chapter02/Formatting/Program.cs
@@ -1,3 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿int numberOfApples = 12;
+decimal pricePerApple = 0.33M;
+Console.WriteLine(
+    format: "{0} apples cost {1:C}",
+    arg0: numberOfApples,
+    arg1: pricePerApple * numberOfApples);
+string formatted = string.Format(
+    format: "{0} apples cost {1:C}",
+    arg0: numberOfApples,
+    arg1: pricePerApple * numberOfApples);
 
-Console.WriteLine("Hello, World!");
+// Three parameter values can use named arguments.
+Console.WriteLine("{0} {1} lived in {2}.",
+    arg0: "Roger", arg1: "Cevung", arg2: "Stockholm");
+// Four or more parameter values cannot us named arguments.
+Console.WriteLine("{0} {1} lived in {2} and worked in the {3} team at {4}.",
+    "roger", "Cevung", "Stockholm", "Education", "Optimizely");
+
+    
