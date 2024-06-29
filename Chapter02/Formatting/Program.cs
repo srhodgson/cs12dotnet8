5a3d700e649095aed2da0f6806d4650c0d8082cf
Stephen Hodgson <stephen@srhodgson.co.uk>
2024-06-29 22:00:42 +0100

    formatting strings and getting inout from users

--- Chapter02/Formatting/Program.cs
+++ Chapter02/Formatting/Program.cs
@@ -1,5 +1,5 @@
 ﻿int numberOfApples = 12;
-decimal pricePerApple = 0.33M;
+decimal pricePerApple = 0.35M;
 Console.WriteLine(
     format: "{0} apples cost {1:C}",
     arg0: numberOfApples,
@@ -16,4 +16,25 @@ Console.WriteLine("{0} {1} lived in {2}.",
 Console.WriteLine("{0} {1} lived in {2} and worked in the {3} team at {4}.",
     "roger", "Cevung", "Stockholm", "Education", "Optimizely");
 
+// The following statement must be all on one line when using C# 10 or earlier. If using C# 11 or later, we can include 
+// a line break in the middle of an expression but not in the string text.
+Console.WriteLine($"{numberOfApples} apples cost {pricePerApple * numberOfApples:C}");
+
+string applesText = "Apples";
+int applesCount = 1234;
+string bananasText = "Bananas";
+int bananasCount = 56789;
+Console.WriteLine();
+Console.WriteLine(format: "{0,-10} {1,6}",
+    arg0: "Name", arg1: "Count");
+Console.WriteLine(format: "{0,-10} {1,6:N0}",
+    arg0: applesText, arg1: applesCount);
+Console.WriteLine(format: "{0,-10} {1,6:N0}",
+    arg0: bananasText, arg1: bananasCount);
+
+Console.Write("Type your first name and press ENTER: ");
+string? firstName = Console.ReadLine();
+Console.Write("Type your age and press ENTER: ");
+string age = Console.ReadLine()!;
+Console.WriteLine($"Hello {firstName}, you look good for {age}.");
     
