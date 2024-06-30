ca2372a7365e06e9f5a017138fcb0fe69f847a6b
Stephen Hodgson <stephen@srhodgson.co.uk>
2024-06-30 22:00:16 +0100

    Update project

--- Chapter02/AsyncConsole/Program.cs
+++ Chapter02/AsyncConsole/Program.cs
@@ -1,3 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
+﻿HttpClient client = new();
+HttpResponseMessage response = await client.GetAsync("http://www.apple.com/");
+WriteLine("Apple's home page has {0:N0} bytes.", response.Content.Headers.ContentLength);
