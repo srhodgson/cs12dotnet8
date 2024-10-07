e86d90b1c1121249fdd388add0bb0ec92ad823c9
Stephen Hodgson <stephen@srhodgson.co.uk>
2024-10-07 22:09:29 +0100

    add new project

---
+++ Chapter02/Variables/Program.cs
@@ -0,0 +1,6 @@
+﻿object height = 1.88; // Storing a double in an object.
+object name = "Steve"; // Storing a string in an object.
+Console.WriteLine($"{name} is {height} meters tall.");
+// int length1 = name.Length; // This gives a compile error 
+int length2 = ((string)name).Length; // Cast name to a string.
+Console.WriteLine($"{name} has {length2} characters.");
