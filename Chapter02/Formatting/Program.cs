e86c1e5f58861e6fcf86dec4908f73376052a757
Stephen Hodgson <stephen@srhodgson.co.uk>
2024-10-08 22:30:56 +0100

    Formatting using interpolated strings

--- Chapter02/Formatting/Program.cs
+++ Chapter02/Formatting/Program.cs
@@ -19,3 +19,7 @@ Console.WriteLine("{0} {1} lived in {2}.",
 Console.WriteLine(
     "{0} {1} lived in {2} and worked in the {3} team at {4}.",
     "Roger", "Cevung", "Stockholm", "Education", "Optimizely");    
+    
+// The following statement must be all one line when using C# 10 or earlier. If using C# 11 or later, we can include a 
+// line break in the middle of an expression but not in the string text.
+Console.WriteLine($"{numberOfApples} apples cost {pricePerApple * numberOfApples:C}");    
