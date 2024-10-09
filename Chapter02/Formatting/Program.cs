bff8f6cc29e6a29f57cb9ac6858e4ff80d960045
Stephen Hodgson <stephen@srhodgson.co.uk>
2024-10-09 22:23:10 +0100

    Accepting input from the user

--- Chapter02/Formatting/Program.cs
+++ Chapter02/Formatting/Program.cs
@@ -37,3 +37,10 @@ Console.WriteLine(format: "{0,-10} {1,6:N0}",
 Console.WriteLine(format: "{0,-10} {1,6:N0}",
     arg0: bananasText, arg1: bananasCount);
 
+Console.WriteLine();
+
+Console.Write("Type your first name and press ENTER: ");
+string? firstName = Console.ReadLine();
+Console.Write("Type your age and press ENTER: ");
+string age = Console.ReadLine()!;
+Console.WriteLine($"Hello {firstName}, you look good for {age}.");
