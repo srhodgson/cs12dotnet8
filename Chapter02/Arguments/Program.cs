39dd0e79d8508be684decd024f5d24a8429d35e8
Stephen Hodgson <stephen@srhodgson.co.uk>
2024-10-09 23:14:25 +0100

    handling platforms that dont support an API

--- Chapter02/Arguments/Program.cs
+++ Chapter02/Arguments/Program.cs
@@ -1,3 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿WriteLine($"There are {args.Length} arguments");
 
-Console.WriteLine("Hello, World!");
+foreach (string arg in args)
+{
+    WriteLine(arg);
+}
+
+if (args.Length < 3)
+{
+    WriteLine($"You must specify two colours and cursor size, e.g.");
+    WriteLine("dotnet run red yellow 50");
+    return; // Stop running.
+}
+ForegroundColor = (ConsoleColor)Enum.Parse(
+    enumType: typeof(ConsoleColor),
+    value: args[0], ignoreCase: true);
+BackgroundColor = (ConsoleColor)Enum.Parse(
+    enumType: typeof(ConsoleColor),
+    value: args[1], ignoreCase: true);
+
+try
+{
+    CursorSize = int.Parse(args[2]);
+}
+catch (PlatformNotSupportedException)
+{
+    WriteLine("The current platform does not support changing the size of the cursor");
+}
+ 
