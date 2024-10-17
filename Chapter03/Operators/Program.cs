921d251e9ec39f9fbcb0fd8c57e7d5db8c417ca0
Stephen Hodgson <stephen@srhodgson.co.uk>
2024-10-17 22:19:42 +0100

    exploring bitwise and binary shift operators

--- Chapter03/Operators/Program.cs
+++ Chapter03/Operators/Program.cs
@@ -26,12 +26,43 @@ bool q = false;
 WriteLine($"AND | p     | q ");
 WriteLine($"p   | {p & p,-5} | {p & q,-5}");
 WriteLine($"q   | {q & p,-5} | {q & q,-5}");
-WriteLine();
-WriteLine($"OR | p     | q ");
+WriteLine($"OR  | p     | q ");
 WriteLine($"p   | {p | p,-5} | {p | q,-5}");
 WriteLine($"q   | {q | p,-5} | {q | q,-5}");
-WriteLine();
 WriteLine($"XOR | p     | q ");
 WriteLine($"p   | {p ^ p,-5} | {p ^ q,-5}");
 WriteLine($"q   | {q ^ p,-5} | {q ^ q,-5}");
 WriteLine();
+WriteLine($"p & DoStuff() = {p & DoStuff()}");
+WriteLine($"q & DoStuff() = {q & DoStuff()}");
+WriteLine();
+WriteLine($"p && DoStuff() = {p && DoStuff()}");
+WriteLine($"q && DoStuff() = {q && DoStuff()}");
+WriteLine();
+int x = 10;
+int y = 6;
+WriteLine($"Expression | Decimal   |   Binary");
+WriteLine($"---------------------------------");
+WriteLine($"x          | {x,7}   | {x:B8}");
+WriteLine($"y          | {y,7}   | {y:B8}");
+WriteLine($"x & y      | {x & y,7}   | {x & y:B8}");
+WriteLine($"x | y      | {x | y,7}   | {x | y:B8}");
+WriteLine($"x ^ y      | {x ^ y,7}   | {x ^ y:B8}");
+// Left-shift x by three bit column 
+WriteLine($"x << 3     | {x << 3,7}   | {x << 3,7:B8}");
+// Multiply x by 8
+WriteLine($"x * 8      | {x * 8,7}   | {x * 8,7:B8}");
+// Right-shift y by one bit column
+WriteLine($"y >> 1    | {y >> 1,7}    |  {y >> 1,7:B8}");
+
+
+
+
+static bool DoStuff()
+{
+    WriteLine("I am doing some stuff.");
+    return true;
+}
+
+
+
