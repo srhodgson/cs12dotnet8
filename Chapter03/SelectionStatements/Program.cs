ab014f21e56dcc68fde33d396f438258fe55b031
Stephen Hodgson <stephen@srhodgson.co.uk>
2024-10-19 22:34:11 +0100

    More switch statementa

--- Chapter03/SelectionStatements/Program.cs
+++ Chapter03/SelectionStatements/Program.cs
@@ -48,3 +48,73 @@ switch (number)
 WriteLine("After end of switch");
 A_label:
     WriteLine($"After A_label");
+
+WriteLine();
+
+var animals = new Animal?[]
+{
+    new Cat {
+        Name = "Karen", 
+        Born = new(year: 2022, month: 8, day: 23),
+        Legs = 4,
+        IsDomestic = true
+        },
+    null,
+    new Cat {
+        Name = "Mufasa",
+        Born = new(year: 1994, month: 6, day: 12)
+    },
+    new Spider {
+        Name = "Sid Vicious",
+        Born = DateTime.Today,
+        IsPoisonous = true
+    },
+    new Spider {
+        Name = "Captain Furry",
+        Born = DateTime.Today
+    }
+};
+
+foreach (Animal? animal in animals)
+{
+    string message;
+    switch (animal)
+    {
+        case Cat fourLeggedCat when fourLeggedCat.Legs == 4:
+            message = $"The cat named {fourLeggedCat.Name} has four legs.";
+            break;
+        case Cat wildCat when wildCat.IsDomestic == false:
+            message = $"The non-domestic cat is named {wildCat.Name}.";
+            break;
+        case Cat cat:
+            message = $"The cat is named {cat.Name}.";
+            break;
+        default: // default is always evaluated last
+            message = $"{animal.Name} is a {animal.GetType().Name}.";
+            break;
+        case Spider spider when spider.IsPoisonous:
+            message = $"The {spider.Name} spider is poisonous. Run!";
+            break;
+        case null:
+            message = "The animal is null.";
+            break;
+        
+        message = animal switch
+        {
+            Cat fourLeggedCat when fourLeggedCat.Legs == 4 
+                => $"The cat named {fourLeggedCat} has four legs.",
+            Cat wildCat when wildCat.IsDomestic == false 
+                => $"The non-domestic cat is name {wildCat.Name}.",
+            Cat cat 
+                => $"The cat name {cat.Name}.",
+            Spider spider when spider.IsPoisonous
+                => $"The {spider.Name} spider is poisonous. Run!",
+            null   
+                => "The animal is null",
+            _ 
+                => $"The {animal.Name} is a {animal.GetType().Name}."
+        };
+        WriteLine($"switch expression: {message}");
+    }
+    WriteLine($"Switch statement: {message}");
+}
