10f85641e52a7fbb4224844e87a3144d99a547a3
Stephen Hodgson <stephen@srhodgson.co.uk>
2024-11-13 22:33:13 +0000

    Add new project

---
+++ Chapter03/CastingConverting/Program.cs
@@ -0,0 +1,32 @@
+﻿using static System.Convert; // To use the ToInt32 method
+
+int a = 10;
+double b = a; // An int can be safely cast into a double.
+WriteLine($"a is {a}, b is {b}");
+
+double c = 9.8;
+int d = (int)c; // Compiler gives an error if you do not explicitly cast.
+WriteLine($"c is {c}, d is {d}"); // d loses the .8 part 
+
+long e = 10;
+int f = (int)e;
+WriteLine($"e is {e:N0}, f is {f:N0}");
+e = 5_000_000_000;
+f = (int)e;
+WriteLine($"e is {e:N0}, f is {f:N0}");
+
+WriteLine();
+
+WriteLine("{0,12} {1,34}", "Decimal", "Binary");
+WriteLine("{0,12} {0,34:B32}", int.MaxValue);
+for (int i = 8; i >= -8; i--) 
+{
+    WriteLine("{0,12} {0,34:B32}", i);
+}
+WriteLine("{0,12} {0,34:B32}", int.MinValue);
+
+WriteLine();
+
+double g = 9.8;
+int h = ToInt32(g); // A method of System.Convert
+WriteLine($"g is {g}, h is {h}");
