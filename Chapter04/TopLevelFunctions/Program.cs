0b5ea7462b256123a5766d4a6ac2e1ee978b9fbc
Stephen Hodgson <stephen@srhodgson.co.uk>
2024-12-01 23:13:07 +0000

    Start new project

---
+++ Chapter04/TopLevelFunctions/Program.cs
@@ -0,0 +1,9 @@
+﻿using static System.Console;
+WriteLine("* Top-Level functions example");
+WhatsMyNamespace(); // Call the function
+
+void WhatsMyNamespace() // Define a local fucntion
+{
+    WriteLine("Namespace of Program class: {0}",
+        arg0: typeof(Program).Namespace ?? "null");
+}
---
+++ Chapter04/WritingFunctions/Program.Functions.cs
@@ -0,0 +1,14 @@
+partial class Program
+{
+    static void TimesTable(byte number, byte size = 12)
+    {
+        WriteLine($"THis is the {number} times table with {size} rows: ");
+        WriteLine();
+        for (int row = 1; row <= size; row++)
+        {
+            WriteLine($"{row} x {number} = {row * number}");
+        }
+
+        WriteLine();
+    }
+}
