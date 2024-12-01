01ebadff837959aa73efb84af726096ddce9b0ec
Stephen Hodgson <stephen@srhodgson.co.uk>
2024-12-01 22:39:20 +0000

    FizzBuzz

---
+++ Chapter03/Ch03Ex04FizzBuzz/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+  if x is divisible by 3 print fizz
+  if x is divisible by 5 print buzz
+  if x is divisible by both print fizzbuzz
+  otherwise print x
+ */
+
+ for (int x = 0; x <= 100; x++)
+ {
+    if (x % 3 == 0 && x % 5 == 0)
+    {
+        Write("FizzBuzz");
+    }
+    else if (x % 5 == 0)
+    {
+        Write("Buzz");
+    }
+    else if (x % 3 == 0)
+    {
+        Write("Fizz");
+    }
+    else 
+    {
+        Write(x);
+    }
+
+    if (x < 100)
+        Write(", ");
+    
+    if (x % 10 == 0)
+        WriteLine();
+ }
