b51268ac045bd1a13e88c1b17b445f954629a1eb
Stephen Hodgson <stephen@srhodgson.co.uk>
2024-12-02 22:31:19 +0000

    writing functions

--- Chapter04/WritingFunctions/Program.Functions.cs
+++ Chapter04/WritingFunctions/Program.Functions.cs
@@ -1,3 +1,5 @@
+using System.Globalization; // To use CultureInfo
+
 partial class Program
 {
     static void TimesTable(byte number, byte size = 12)
@@ -8,7 +10,33 @@ partial class Program
         {
             WriteLine($"{row} x {number} = {row * number}");
         }
-
         WriteLine();
+    }
+
+    static decimal CalculateTax(decimal amount, string twoLetterRegionCode)
+    {
+        decimal rate = twoLetterRegionCode switch
+        {
+            "CH" => 0.08M, // Switzerland
+            "DK" or "NO" => 0.25M, // Denmark, Norway
+            "GB" or "FR" => 0.2M, // UK, France
+            "HU" => 0.27M, // Hungary
+            "OR" or "AK" or "MT" => 0.0M, // Oregon, Alaska, Montana
+            "ND" or "WI" or "ME" or "VA" => 0.05M,
+            "CA" => 0.0825M, // California
+            _ => 0.06M // Most other states
+        };
+        return amount * rate;
+    }
+
+    static void ConfigureConsole(string culture = "en-US", bool useCOmputerCulture = false)
+    {
+        // To enable Unicode characters like Euro symbol in the console 
+        OutputEncoding = System.Text.Encoding.UTF8;
+        if (!useCOmputerCulture)
+        {
+            CultureInfo.CurrentCulture = CultureInfo.GetCultureInfo(culture);
+        }
+        WriteLine($"CurrentCulture: {CultureInfo.CurrentCulture.DisplayName}");
     }
 }
--- Chapter04/WritingFunctions/Program.cs
+++ Chapter04/WritingFunctions/Program.cs
@@ -1,1 +1,8 @@
-﻿TimesTable(7);
+﻿//TimesTable(7);
+
+ConfigureConsole();
+
+decimal taxToPay = CalculateTax(amount: 149, twoLetterRegionCode: "FR");
+WriteLine($"You must pay {taxToPay:C} in tax.");
+// Alternatively, call function in the interpolated string.
+// WriteLine($"You must pay {CalculateTax(amount: 149, twoLetterRegionCode: "FR"):C} in tax.");
