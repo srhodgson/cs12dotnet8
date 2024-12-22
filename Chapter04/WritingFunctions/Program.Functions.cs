8344fb8ab15c7623d2530890e306f5dbeb54551f
Stephen Hodgson <stephen@srhodgson.co.uk>
2024-12-22 22:24:45 +0000

    cardinal to ordinal

--- Chapter04/WritingFunctions/Program.Functions.cs
+++ Chapter04/WritingFunctions/Program.Functions.cs
@@ -29,14 +29,46 @@ partial class Program
         return amount * rate;
     }
 
-    static void ConfigureConsole(string culture = "en-US", bool useCOmputerCulture = false)
+    static void ConfigureConsole(string culture = "en-US", bool useComputerCulture = false)
     {
         // To enable Unicode characters like Euro symbol in the console 
         OutputEncoding = System.Text.Encoding.UTF8;
-        if (!useCOmputerCulture)
+        if (!useComputerCulture)
         {
             CultureInfo.CurrentCulture = CultureInfo.GetCultureInfo(culture);
         }
         WriteLine($"CurrentCulture: {CultureInfo.CurrentCulture.DisplayName}");
     }
+    
+    static string CardinalToOrdinal(uint number)
+    {
+        uint lastTwoDigits = number % 100;
+        switch (lastTwoDigits)
+        {
+            case 11: // Special cases for 11th to 13th
+            case 12:
+            case 13:
+                return $"{number:N0}th";
+            default:
+                uint lastDigit = number % 10;
+                string suffix = lastDigit switch 
+                {
+                    1 => "st",
+                    2 => "nd",
+                    3 => "rd",
+                    _ => "th"
+                };
+                return $"{number:N0}{suffix}";    
+        }
+    }
+
+    static void RunCardinalToOrdinal()
+    {
+        for (uint number = 1; number <= 1500; number++)
+        {
+            Write($"{CardinalToOrdinal(number)} ");
+        }
+        WriteLine();
+    }
 }
+
--- Chapter04/WritingFunctions/Program.cs
+++ Chapter04/WritingFunctions/Program.cs
@@ -1,8 +1,10 @@
 ﻿//TimesTable(7);
 
-ConfigureConsole();
+//ConfigureConsole(culture: "fr-FR");
 
-decimal taxToPay = CalculateTax(amount: 149, twoLetterRegionCode: "FR");
-WriteLine($"You must pay {taxToPay:C} in tax.");
+// decimal taxToPay = CalculateTax(amount: 149, twoLetterRegionCode: "FR");
+// WriteLine($"You must pay {taxToPay:C} in tax.");
 // Alternatively, call function in the interpolated string.
 // WriteLine($"You must pay {CalculateTax(amount: 149, twoLetterRegionCode: "FR"):C} in tax.");
+
+RunCardinalToOrdinal();
