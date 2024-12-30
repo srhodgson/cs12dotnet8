4975f68ff6325cec50753795ee52b410f05aef78
Stephen Hodgson <stephen@srhodgson.co.uk>
2024-12-30 23:26:34 +0000

    Add new solution to look at Debugging

---
+++ Chapter04/Debugging/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+double a = 4.5;
+double b = 2.5;
+double answer = Add(a, b);
+WriteLine($"{a} + {b} = {answer}");
+WriteLine("Press ENTER to end the app.");
+ReadLine(); // Wait for the user to press Enter 
+
+double Add(double a, double b)
+{
+    return a * b; // Deliberate bug
+}
