8757830a159ba8a0b244f56b29acb996232d9700
Stephen Hodgson <stephen@srhodgson.co.uk>
2025-01-17 22:32:43 +0000

    hot reloading

--- Chapter04/Debugging/Program.cs
+++ Chapter04/Debugging/Program.cs
@@ -1,13 +1,18 @@
 ﻿using System.ComponentModel;
 
-double a = 4.5;
-double b = 2.5;
-double answer = Add(a, b);
-WriteLine($"{a} + {b} = {answer}");
-WriteLine("Press ENTER to end the app.");
-ReadLine(); // Wait for the user to press Enter 
 
-double Add(double a, double b)
-{
-    return a * b; // Deliberate bug
-}
+Write("Enter a number: ");
+string number = ReadLine()!;
+double a = double.Parse(number);
+
+// double a = 4.5;
+// double b = 2.5;
+// double answer = Add(a, b);
+// WriteLine($"{a} + {b} = {answer}");
+// WriteLine("Press ENTER to end the app.");
+// ReadLine(); // Wait for the user to press Enter 
+
+// double Add(double a, double b)
+// {
+//     return a * b; // Deliberate bug
+// }
