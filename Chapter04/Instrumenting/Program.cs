33d6227f9632399efe44b0a45224069bca56a412
Stephen Hodgson <stephen@srhodgson.co.uk>
2025-02-04 22:09:03 +0000

    debugging and tracing

---
+++ Chapter04/Instrumenting/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics; // To use Debug and Trace
+
+string logPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory),
+"log.text");
+Console.WriteLine($"Writing to: {logPath}");
+TextWriterTraceListener logFile = new(File.CreateText(logPath));
+Trace.Listeners.Add(logFile);
+
+#if DEBUG
+// Text writer is buffered, so this option calls
+// Flush() on all listeners after writing 
+Trace.AutoFlush = true;
+#endif
+
+Debug.WriteLine("Debug says, I am watching!");
+Debug.WriteLine("Trace says, I am watching!");
+
+// Close the text file (also flushes) and release resources.
+Debug.Close();
+Trace.Close();
