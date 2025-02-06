2753a84bdac992201bebec2c841a02509760e99e
Stephen Hodgson <stephen@srhodgson.co.uk>
2025-02-06 18:05:01 +0000

    config files

--- Chapter04/Instrumenting/Program.cs
+++ Chapter04/Instrumenting/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics; // To use Debug and Trace
+using Microsoft.Extensions.Configuration; // To use Configuration-Builder 
 
 string logPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory),
 "log.text");
@@ -15,6 +16,21 @@ Trace.AutoFlush = true;
 Debug.WriteLine("Debug says, I am watching!");
 Debug.WriteLine("Trace says, I am watching!");
 
+string settingsFile = "appsettings.json";
+string settingsPath = Path.Combine(Directory.GetCurrentDirectory(), settingsFile);
+Console.WriteLine("Processing: {0}", settingsPath);
+Console.WriteLine("--{0} contents--", settingsFile);
+Console.WriteLine(File.ReadAllText(settingsPath));
+Console.WriteLine("----");
+ConfigurationBuilder builder = new();
+builder.SetBasePath(Directory.GetCurrentDirectory());
+// Add the settings file to the processed configuration and make it mandatory so
+// an exception will be thrown if the file is not found.
+builder.AddJsonFile(settingsFile, optional: false, reloadOnChange: true);
+IConfigurationRoot configuration = builder.Build();
+TraceSwitch ts = new(
+    displayName: "PacktSwitch",
+    description: "This switch is set via a JSON config");
 // Close the text file (also flushes) and release resources.
 Debug.Close();
 Trace.Close();
