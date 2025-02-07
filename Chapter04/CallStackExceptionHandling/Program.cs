15a22f4568c34107bfa8ca1a4a6e2e1fc828de1c
Stephen Hodgson <stephen@srhodgson.co.uk>
2025-02-07 23:15:26 +0000

    Throwing exceptions

---
+++ Chapter04/CallStackExceptionHandling/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using CallStackExceptionHandlingLib; // To use Processor
+
+using static System.Console;
+
+WriteLine("In Main");
+Alpha();
+void Alpha()
+{
+    WriteLine("In Alpha");
+    Beta();
+}
+void Beta()
+{
+    WriteLine("In Beta");
+    try
+    {
+        Processor.Gamma();
+    }
+    catch (Exception ex)
+    {
+        WriteLine($"Caught this: {ex.Message}");
+        throw;
+    }
+    
+}
