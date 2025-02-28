5f624593789af4bd5d0bf0ad192bbd4a2f71a24a
Stephen Hodgson <stephen@srhodgson.co.uk>
2025-02-28 23:15:39 +0000

    Optimize flight calculation using pattern matching
    - Simplified the switch expression for determining flight costs.
    - Removed redundant array initialization.
    - Improved readability and maintainability of the code.

--- Chapter05/PeopleApp/Program.cs
+++ Chapter05/PeopleApp/Program.cs
@@ -247,12 +247,24 @@ foreach (Passenger passenger in passengers)
 {
     decimal flightCost = passenger switch
     {
+        /* C# 8 Syntax
         FirstClassPassenger p when p.AirMiles > 35_000 => 1_500M,
         FirstClassPassenger p when p.AirMiles > 15_000 => 1_750M,
         FirstClassPassenger _ => 2_000M,
         BusinessClassPassenger _ => 1_000M,
         CoachClassPassenger p when p.CarryOnKG < 10.0 => 500M,
         CoachClassPassenger _ => 650M,
+        _ => 800M*/
+        // C# 9 or later syntax 
+        FirstClassPassenger p => p.AirMiles switch
+        {
+            > 35_000 => 1_500M,
+            > 15_000 => 1_750M,
+            _ => 2_000M
+        },
+        BusinessClassPassenger => 1_000M,
+        CoachClassPassenger p when p.CarryOnKG < 10.0 => 500M,
+        CoachClassPassenger => 650M,
         _ => 800M
     };
     WriteLine($"Flight costs {flightCost:C} for {passenger}");
