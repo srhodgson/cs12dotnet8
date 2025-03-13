7796d12b6bd04bb3ec41f4f36f3cd508eecd1302
Stephen Hodgson <stephen@srhodgson.co.uk>
2025-03-13 22:08:16 +0000

    Looking at records
    Add ImmutablePerson and ImmutableVehicle types
    
    Updated Program.cs to create and use ImmutablePerson and ImmutableVehicle.
    Introduced an instance of ImmutablePerson named 'jeff' and an ImmutableVehicle instance 'car' with specific attributes.
    Implemented a 'with' expression to create a modified version of 'car' and output the original and new car colors.
    
    Added a new namespace 'Packt.Shared' in Record.cs, defining the ImmutablePerson class and ImmutableVehicle record with appropriate properties.

---
+++ Chapter05/PacktLibraryModern/Record.cs
@@ -0,0 +1,14 @@
+﻿namespace Packt.Shared;
+
+public class ImmutablePerson
+{
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+}
+
+public record ImmutableVehicle
+{
+    public int Wheels { get; init; }
+    public string? Color { get; init; }
+    public string? Brand { get; init; }
+}
--- Chapter05/PeopleApp/Program.cs
+++ Chapter05/PeopleApp/Program.cs
@@ -270,3 +270,21 @@ foreach (Passenger passenger in passengers)
     WriteLine($"Flight costs {flightCost:C} for {passenger}");
 }
 
+WriteLine();
+
+ImmutablePerson jeff = new()
+{
+    FirstName = "Jeff",
+    LastName = "Winger"
+};
+//jeff.FirstName = "Geoff";
+
+ImmutableVehicle car = new()
+{
+    Brand = "Mazda MX-5 RF",
+    Color = "Soul Red Crystal Metallic",
+    Wheels = 4
+};
+ImmutableVehicle repaintedCar = car with { Color = "Polymetal Grey Metallic" };
+WriteLine($"Original car color was {car.Color}");
+WriteLine($"New car color is {repaintedCar.Color}");
