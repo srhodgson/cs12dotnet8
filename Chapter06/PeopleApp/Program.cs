cf9f72253b06e724b090ec2d357d1436644a936d
Stephen Hodgson <stephen@srhodgson.co.uk>
2025-03-20 23:17:15 +0000

    Start adding project files for chapter 6

---
+++ Chapter06/PeopleApp/Program.cs
@@ -0,0 +1,8 @@
+﻿using Packt.Shared;
+
+Person harry = new()
+{
+    Name = "Harry",
+    Born = new(year: 2001, month: 3, day: 25, hour: 0, minute: 0, second: 0, offset: TimeSpan.Zero)
+};
+harry.WriteToConsole();
